d01c657913a16de0283a797ae5badda0d0932c71
csoft2015 <kodashima.mizuki@csoft.co.jp>
2023-10-06 11:42:00 +0900

    お試しコミット

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,65 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+// import { NextPage } from "next";
+// import { useEffect, useState } from "react";
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+import styles from "./index.module.css";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+// getServerSidePropsから渡されるpropsの型
+type Props = {
+    initialImageUrl: string;
+}
 
-export default IndexPage
+// ページコンポーネント関数にpropsを受け取る引数を追加する
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+    // 1. useStateを使って状態を定義する
+    // const [imageUrl, setImageUrl] = useState("");
+    const [imageUrl, setImageUrl] = useState(initialImageUrl); // 初期値を渡す
+    // const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false); // 初期状態はfalseにしておく
+
+    // 2. マウント時に画像を読み込む宣言
+    // useEffect(() => {
+    //     fetchImage().then((newImage) => {
+    //         setImageUrl(newImage.url); // 画像URLの状態を更新する
+    //         setLoading(false); // ローディング状態を更新する
+    //     });
+    // }, []); 
+    // ボタンをクリックした時に画像を読み込む処理
+    const handleClick = async() => {
+        setLoading(true); // 読込中フラグを立てる
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url); //画像URLの状態を更新する
+        setLoading(false); // 読込中フラグを倒す
+    };
+    return (
+        <div className={styles.page}>
+            <button onClick={handleClick} className={styles.button}>
+                他のにゃんこも見る
+            </button>
+            <div className={styles.frame}>
+                {loading || <img src={imageUrl} />}
+            </div>
+        </div>
+    );
+};
+export default IndexPage;
+
+// サーバーサイドで実行する処理
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const image = await fetchImage();
+    return {
+        props: {
+            initialImageUrl: image.url,
+        },
+    };
+};
+
+type Image = {
+    url: string;
+}
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    console.log(images);
+    return images[0];
+}
